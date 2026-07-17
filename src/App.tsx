52b11649c75a2771aecdf56b5b0ca139c0b12a45
stelcreativ <45742221+stelcreativ@users.noreply.github.com>
2026-07-17 09:04:59 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Navbar } from './components/Navbar';
+import './index.css'
+
+function App() {
+
+  return (
+    <div className="bg-slate-900 text-slate-100 min-h-screen overflow-x-hidden">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
